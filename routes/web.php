073464a4adc52b62c329780f09c897b93add03a9
DiegoNeres99 <miottadiego@gmail.com>
2025-11-03 16:40:07 -0300

    tela pedido

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ShowController;
 
+Route::get('/', function () {
+    return redirect()->route('products.index');
+});
 
 Route::resource('products', ProductController::class);
 Route::resource('customers', CustomerController::class);
 Route::resource('categories', CategoryController::class);
+Route::resource('orders', OrderController::class);
